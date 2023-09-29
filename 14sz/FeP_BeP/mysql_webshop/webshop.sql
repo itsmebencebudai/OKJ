7dae9677fa3dffa69c2df2e990064426055c9d92
itsmebencebudai <bencebudai2004@gmail.com>
2023-09-29 11:11:43 +0200

    webshop changed

--- 14sz/FeP_BeP/mysql_webshop/webshop.sql
+++ 14sz/FeP_BeP/mysql_webshop/webshop.sql
@@ -1,8 +1,10 @@
+-- Active: 1695968358990@@127.0.0.1@3306@webshop
+
 CREATE DATABASE
     webshop DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci
 
 CREATE Table
-    Users_(
+    `Users`(
         id int(10) NOT NULL AUTO_INCREMENT,
         name varchar(50) NOT NULL,
         email varchar(50) NOT NULL,
@@ -10,57 +12,62 @@ CREATE Table
         shipping int DEFAULT NULL,
         addressId int DEFAULT NULL,
         account_number VARCHAR(16),
-        PRIMARY KEY (user_id),
-        constraint FK_users_address_addressId FOREIGN (addressId) REFERENCES address_ (addressId),
+        PRIMARY KEY (id),
+        FOREIGN KEY(addressId) REFERENCES `Address` (id)
     )
 
 CREATE Table
-    Address_(
-        addressId int DEFAULT NULL AUTO_INCREMENT,
+    `Address`(
+        id int DEFAULT NULL AUTO_INCREMENT,
         street VARCHAR(50) Not NULL,
         city VARCHAR(20) Not NULL,
         zip_code int DEFAULT NULL,
         delivery_address BOOLEAN,
-        PRIMARY KEY (id),
+        PRIMARY KEY (id)
     )
 
-CREATE products(
-    id int(10) NOT NULL AUTO_INCREMENT,
-    product_name varchar(50) NOT NULL,
-    product_description TEXT NOT NULL,
-    product_price int DEFAULT NULL,
-    stock int DEFAULT NULL,
-    PRIMARY KEY (id)
-)
+CREATE TABLE
+    `products`(
+        id int(10) NOT NULL AUTO_INCREMENT,
+        product_name varchar(50) NOT NULL,
+        product_description TEXT NOT NULL,
+        product_price int DEFAULT NULL,
+        stock int DEFAULT NULL,
+        PRIMARY KEY (id)
+    )
 
 CREATE Table
-    cart (
+    `cart` (
         id int(10) NOT NULL AUTO_INCREMENT,
         userId int DEFAULT NULL,
         date DATETIME,
         PRIMARY KEY (id),
-        constraint FK_cart_users_userId FOREIGN KEY (userId) REFERENCES users (id)
+        FOREIGN KEY (userId) REFERENCES `Users` (id)
     )
+
 CREATE TABLE
-    cartitems (
+    `cartitems` (
         cartId INT DEFAULT NULL,
         productId INT DEFAULT NULL,
         quantity INT DEFAULT NULL,
-        constraint FK_cartitems_productsId FOREIGN KEY (productId) REFERENCES products (id)
-        constraint FK_cartitems_cartId FOREIGN KEY (cartId) REFERENCES cart (id)
+        FOREIGN KEY (cartId) REFERENCES `cart` (id),
+        FOREIGN KEY (productId) REFERENCES `products` (id)
     )
 
 CREATE Table
-    invoice (
+    `invoice` (
         id int(10) NOT NULL AUTO_INCREMENT,
         userId int DEFAULT NULL,
         date DATETIME,
         total int DEFAULT NULL,
-        constraint FK_invoice_user_userId FOREIGN KEY (userid) REFERENCES user (id)
+        PRIMARY KEY (id),
+        FOREIGN KEY (userId) REFERENCES `Users` (id)
     )
 
 CREATE Table
     invoce_items(
-        invoice_id INT DEFAULT NULL,
-        product_id INT DEFAULT NULL,
+        invoiceId INT DEFAULT NULL,
+        productId INT DEFAULT NULL,
+        constraint FK_invoice_items_invoice_invoiceId FOREIGN KEY (invoiceId) REFERENCES invoice (id),
+        constraint FK_invoice_items_products_productId FOREIGN KEY (productId) REFERENCES products (id)
     )
