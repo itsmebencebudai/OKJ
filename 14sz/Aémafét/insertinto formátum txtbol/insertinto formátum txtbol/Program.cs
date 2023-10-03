85661bd1bef2616ba85f06f59f63d81f9f503a03
itsmebencebudai <bencebudai2004@gmail.com>
2023-10-03 11:51:39 +0200

    10.03 dolgozat + insertinto prog hogyne szenvedjek

---
+++ 14sz/Aémafét/insertinto formátum txtbol/insertinto formátum txtbol/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace insertinto_formátum_txtbol
+{
+    class Program
+    {
+        static void Main()
+        {
+            // A beolvasandó és módosítandó fájl elérési útja
+            string inputFile = @"C:\Users\budai.bence\Desktop\OKJ\14sz\Aémafét\10.03 dolgozat\insertinto.txt";
+            string outputFile = @"C:\Users\budai.bence\Desktop\OKJ\14sz\Aémafét\10.03 dolgozat\jóinsertinto.txt";
+
+            try
+            {
+                // Ellenőrizzük, hogy a bemeneti fájl létezik-e
+                if (File.Exists(inputFile))
+                {
+                    // Fájl beolvasása
+                    string[] lines = File.ReadAllLines(inputFile);
+
+                    // Módosított sorok létrehozása
+                    string[] modifiedLines = new string[lines.Length];
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        modifiedLines[i] = "\"" + lines[i] + "\" +";
+                    }
+
+                    // Módosított sorok írása az output fájlba
+                    File.WriteAllLines(outputFile, modifiedLines);
+
+                    Console.WriteLine("Módosított fájl elkészült.");
+                }
+                else
+                {
+                    Console.WriteLine("A bemeneti fájl nem található.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Hiba történt: {ex.Message}");
+            }
+        }
+    }
+}
