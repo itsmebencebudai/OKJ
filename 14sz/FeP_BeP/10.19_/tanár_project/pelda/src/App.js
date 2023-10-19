f59ad3905c34c9c042f91e6677f6ec3823fee8c9
itsmebencebudai <bencebudai2004@gmail.com>
2023-10-19 15:24:30 +0200

    10.19 15:24

--- 14sz/FeP_BeP/10.19_/tanár_project/pelda/src/App.js
+++ 14sz/FeP_BeP/10.19_/tanár_project/pelda/src/App.js
@@ -5,50 +5,71 @@ const app = express();
 const port = 8000;
 
 //Betölti a CORS támogatást 
-app.use(cors({origin: '*'}));
+app.use(cors({ origin: '*' }));
 
 //lehetővé teszi a POST kérések elküldött adatainak (body) elérését
 app.use(express.json());
 
 
 // ez a végpont mutatja, hogy fut a node js szerver
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     res.send("<h1>Szerver fut</h1>")
 })
 
 // ez a végpont visszaadja az összes felhasználó adatát
-app.get('/getdata',(req,res) => {
-    var con = mysql.createConnection({host:"localhost",user:"root",password:"",database:"webshop_tanar"});
-    con.connect(function(err) {
+app.get('/getdata', (req, res) => {
+    var con = mysql.createConnection({ host: "localhost", user: "root", password: "", database: "webshop_tanar" });
+    con.connect(function (err) {
         if (err) throw err;
         console.log('sikeres csatlakozás');
     })
-    con.query('select * from User', (err,result) =>{
+    con.query('select * from User', (err, result) => {
         if (err) throw err;
         res.send(result);
-    })   
+    })
 })
 
+
+// ez a végpont visszaadja egy felhasználó adatát
+app.get('/getuserdata', (req, res) => {
+    const userId = 2;
+    if (!userId) {
+        return res.status(400).json({ error: 'UserID is required' });
+    }
+    const sql = 'SELECT * FROM User WHERE userID = ?';
+    pool.query(sql, [userId], (err, result) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'An error occurred while fetching user data' });
+        }
+        if (result.length === 0) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        res.json(result[0]);
+    });
+});
+
+
 // Ez a végpont regisztrál egy új felhasználót és lementi az adatbázisba
-app.post('/reg', (req,res) => {
-    var con = mysql.createConnection({host:"localhost",user:"root",password:"",database:"webshop_tanar"});
-    con.connect(function(err) {
+app.post('/reg', (req, res) => {
+    var con = mysql.createConnection({ host: "localhost", user: "root", password: "", database: "webshop_tanar" });
+    con.connect(function (err) {
         if (err) throw err;
         console.log('sikeres csatlakozás');
     })
-    const sql = 'insert into User (name,email,password,accountNumber) values ("' +req.body.name + '","'+ req.body.email+'","'+ req.body.password+'","'+ req.body.accountNumber+'")';
+    const sql = 'insert into User (name,email,password,accountNumber) values ("' + req.body.name + '","' + req.body.email + '","' + req.body.password + '","' + req.body.accountNumber + '")';
     const sql2 = 'insert into User (name,email,password,accountNumber) values (?,?,?,?)';
-    
+
     console.log(sql);
-    con.query(sql2,[req.body.name,req.body.email,req.body.password,req.body.accountNumber], (err,result) =>{
+    con.query(sql2, [req.body.name, req.body.email, req.body.password, req.body.accountNumber], (err, result) => {
         if (err) res.send(err);
         res.send(result);
-    })   
+    })
 })
 
 // publikáljuk a szervert
 app.listen(port, () => {
-console.log(`Példa alkalmazás publikálva ${port}-on`);
+    console.log(`Példa alkalmazás publikálva ${port}-on`);
 
 })
 
--- 14sz/FeP_BeP/10.19_/tanár_project/pelda/src/regisztracio/reg.js
+++ 14sz/FeP_BeP/10.19_/tanár_project/pelda/src/regisztracio/reg.js
@@ -1,12 +1,19 @@
 regDataSave = (data) => {
-    postData("http://localhost:8000/reg",data).then((response) => {
-        console.log(response.status);
-        console.log(headers);
-    return response.json();
-        }).then((data) => {
-            console.log(data)
-        }).catch((error)=>{
-            console.log(error)
-            alert("hiba");
-        })
+    let loading = document.getElementById("loading");
+    loading.style.visibility = "visible";
+    postData("http://localhost:8000/reg", data).then((response) => {
+        if (response.status == 404) {
+            err = document.getElementById('error');
+            err.innerHtml = "hiba";
+            throw err.innerHtml = "hiba";
+        }
+        return response.json();
+    }).then((data) => {
+        console.log(data)
+    }).catch((error) => {
+        console.log(error)
+        alert("hiba");
+    }).finally(()=>{
+        loading.style.visibility="hidden";
+    })
 }
