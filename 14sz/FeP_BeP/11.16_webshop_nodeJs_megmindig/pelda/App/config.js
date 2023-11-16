0005d29f5542c5a70d6b6a235c550d9c6394047f
itsmebencebudai <bencebudai2004@gmail.com>
2023-11-16 09:40:46 +0100

    11.16 9:40 webshop_nodeJs_megmindig (1)

---
+++ 14sz/FeP_BeP/11.16_webshop_nodeJs_megmindig/pelda/App/config.js
@@ -0,0 +1,14 @@
+const config = module.exports
+
+
+class Config {
+    user = "root";
+    password = "";
+    database = "webshop_tanar3";
+    host = "localhost";
+    constructor() {
+        return { host: this.host, user: this.user, password: this.password, database: this.database };
+    }
+}
+
+config.database = new Config(); 
---
+++ 14sz/FeP_BeP/11.16_webshop_nodeJs_megmindig/pelda/addr/router.js
@@ -0,0 +1,6 @@
+var address = require('./addr-model')
+var router = require('express').Router()
+
+router.post('/address/:id', (req, res) => { address.newAddress(req, res) });
+
+module.exports = router
