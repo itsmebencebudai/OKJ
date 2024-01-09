f2b92fb47f4fba3c0c86a0b843bc2192b944af4d
itsmebencebudai <bencebudai2004@gmail.com>
2024-01-09 14:39:19 +0100

    mozi WPF

---
+++ 14sz/Aémafét/01.09_mozi_WPF/mozi-WPF/mozi-WPF/frmFilm.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace mozi_WPF
+{
+    /// <summary>
+    /// Interaction logic for frmFilm.xaml
+    /// </summary>
+    public partial class frmFilm : Window
+    {
+        public frmFilm()
+        {
+            InitializeComponent();
+            Uri iconUri = new Uri("images/mozi.ico", UriKind.RelativeOrAbsolute);
+            this.Icon = BitmapFrame.Create(iconUri);
+
+        }
+    }
+}
